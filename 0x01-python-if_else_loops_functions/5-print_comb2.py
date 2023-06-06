358f4677290f532a57012d94d53b48f7093ace36
rohoboy <petergicheha39@gmail.com>
2023-06-06 09:15:52 -0700

    5. 00...99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,6 +2,5 @@
 for i in range(0, 100):
     if (i == 99):
         print("{}".format(i))
-    else:
-        print("{:02}".format(i), end=", ")
-
+        continue
+    print("{:02}".format(i), end=", ")
